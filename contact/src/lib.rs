8a7214e09aad81b1a89b994f64dde0e61dfd1c93
vktornaj <victor2211812@gmail.com>
2024-03-08 18:26:18 +0000

    add: contact and base feature

---
+++ contact/src/lib.rs
@@ -0,0 +1,13 @@
+mod domain;
+mod application;
+mod adapter;
+
+// Adapter layer
+// pub use adapter::driving::web::{handlers, schemas};
+// pub use adapter::driven::cache::redis::token_cache::TokenCache;
+// Application layer
+// pub use application::port::driven::token_cache::TokenCacheTrait;
+// pub use application::use_cases::{
+//     create_single_use_token,
+//     authenticate_single_use_token,
+// };
